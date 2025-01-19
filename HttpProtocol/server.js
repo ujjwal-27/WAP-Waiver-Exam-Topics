c9e5aa035d81c83530d38d2e9c3fba5a3ea50106
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-19 11:56:56 +0545

    implement server close to kill port

--- HttpProtocol/server.js
+++ HttpProtocol/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+// const path = require('path');
 
 const app = express();
 
@@ -6,7 +7,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express');
+    res.send('<h1>Hello from Express</h1>');
 });
 
-app.listen(5000, () => console.log("Server started on 5000"));
+const server = app.listen(5000, () => {
+    console.log('Server started on 5000');
+});
+
+// Listen for shutdown signal and gracefully close the server
+process.on('SIGINT', () => {
+    console.log('Server shutting down...');
+    server.close(() => {
+        console.log('Server closed');
+        process.exit(0);
+    });
+});
