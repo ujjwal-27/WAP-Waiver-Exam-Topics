d35f3ac5a7d10fea7f233fb5d987cede54d0fba7
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-22 09:48:05 +0545

    add comments

--- JavaScriptES6/varLetConst/index.js
+++ JavaScriptES6/varLetConst/index.js
@@ -1,3 +1,8 @@
+/**
+ * var -> function scoped
+ * Variable 'i' is defined within for-loop, but it is still accessible outside for-loop scope.
+ * It is accessible within the scope of function.
+ */
 function testVar() {
     for (var i = 0; i < 5; i++) {
         console.log(i);
@@ -8,6 +13,11 @@ function testVar() {
 
 testVar();
 
+/**
+ * let -> block scoped
+ * Variable 'i' is defined within for-loop.
+ * It is not accessible outside for-loop scope .
+ */
 function testLet() {
     for (let i = 0; i < 5; i++) {
         console.log(i);
@@ -18,6 +28,10 @@ function testLet() {
 
 testLet();
 
+/**
+ * const -> block scoped
+ * The value of variable cannot be changed if defined using 'const'
+ */
 function testConst() {
     const i = 7;
 
