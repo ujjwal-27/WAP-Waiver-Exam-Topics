9c7a9bc4c38dc74f7087b44d944d4bd837cc3752
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-23 08:30:12 +0545

    understand objects in javascript

---
+++ JavaScriptES6/objects/index.js
@@ -0,0 +1,24 @@
+/**
+ * Objects are the collection of key-value pairs in JS
+ * 
+ * Here, we have an object named 'athlete' with two properties 'name' and 'age', and two methods 'run()' and 'jump()'.
+ * 'this' keyword is used for accessing the property within the object
+ */
+let athlete = {
+    name: 'Ujjwal',
+    age: 27,
+    run(distance) {
+        return `${this.name}, who is ${this.age} years young, is running ${distance} km.`;
+    },
+    jump(height) {
+        return `${this.name}, who is ${this.age} years young, is jumping ${height} ft. high.`;
+    }
+}
+
+let targetMember = 'name';
+athlete[targetMember] = 'Jordon';
+
+console.log(athlete.run(22));
+console.log(athlete.jump(7));
+
+
