3f71a801a9710d00ac18cdeafbe886ea5e232539
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-28 08:03:23 +0545

    add  comment

--- JavaScriptES6/arrayMap/script.js
+++ JavaScriptES6/arrayMap/script.js
@@ -6,7 +6,8 @@ let colors = ['white', 'green', 'red'];
 // })
 
 // Using arrow function on callback function. Basically, ''arrow function'' is always preferred on 'callback functions'.
-// Map function returns a new arrya, so it must be stored in a variable.
+// Map method returns a new arrya, so it must be stored in a variable.
+// This map method is very useful in React while rendering list.
 let mappedColors = colors.map(color => `<li> ${color} </li>`);
 
 console.log(mappedColors);
