f711dcb13222272ea89343511ace5c42eaa8134d
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-28 07:58:06 +0545

    add comments

--- JavaScriptES6/arrayMap/script.js
+++ JavaScriptES6/arrayMap/script.js
@@ -1,9 +1,11 @@
 let colors = ['white', 'green', 'red'];
 
-colors.map(function (color) {
-    return '<li>' + color + '</li>';
-})
+// Using regular function 
+// colors.map(function (color) {
+//     return '<li>' + color + '</li>';
+// })
 
+// Using arrow function on callback function. Basically, ''arrow function'' is always preferred on 'callback functions'.
 colors.map(color => `<li> ${color} </li>`);
 
 console.log(colors);
