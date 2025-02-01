9c379d03611f35ce3d7bbb5e2b437e04291bd0a7
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-01 20:01:43 +0545

    understand super() method concept

--- JavaScriptES6/inheritance/script.js
+++ JavaScriptES6/inheritance/script.js
@@ -9,16 +9,27 @@ class Person {
 }
 
 /**
- * Here, the Teacher class is inheriting all the properties and methods of class Person
+ * Here, the Teacher class is inheriting all the properties and methods of class Person.
  */
 class Teacher extends Person {
+    /**
+     * The constructor takes two parameters: name and degree.
+     * Since, 'name' is already the property of 'Person' class, it must be passed through the constructor of 'Person' class using 'super()' method.
+     * @param {String} name 
+     * @param {String} degree 
+     */
+    constructor(name, degree) {
+        super(name); // this calls the constructor of a parent class
+        this.degree = degree;
+    }
+
     teach() {
-        console.log(`${this.name} is teaching.`);
+        console.log(`${this.name} teaches ${this.degree} courses.`);
 
     }
 }
 
-const teacher = new Teacher("Ujjwal");
+const teacher = new Teacher("Ujjwal", "MSc");
 const teach = teacher.teach.bind(teacher); // binding 'this' keyword with teacher object
 teach(); // standalone function
 
