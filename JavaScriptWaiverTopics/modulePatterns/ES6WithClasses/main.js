f38097bb7739f1c844c08f9bd0fbb5ea7442aeb0
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-12 14:40:09 +0545

    Revert "JS Module Patterns: ES6 with Classes"

--- JavaScriptWaiverTopics/modulePatterns/ES6WithClasses/main.js
+++
@@ -1,9 +0,0 @@
-import { ShoppingCart } from "./cart.js";
-
-const shoppingCart = new ShoppingCart();
-
-shoppingCart.addCart("gloves", 5000);
-shoppingCart.addCart("wraps", 1000);
-shoppingCart.addCart("guard", 2500);
-
-shoppingCart.listItems();
